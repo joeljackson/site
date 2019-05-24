94f4512d5d98ebc7ce4328af9804e18ca8e6cadf
Joel Jackson <jackson.joel@gmail.com>
2019-05-24 13:47:27 -0700

    Initial styling

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -2,22 +2,9 @@ import * as React from 'react'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
-import './index.css'
-
 const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
+  <div>
+    <div>
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
@@ -52,14 +39,7 @@ class DefaultLayout extends React.PureComponent<DefaultLayoutProps, void> {
           ]}
         />
         <Header />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
+        <div>
           {this.props.children()}
         </div>
       </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import Link from 'gatsby-link'
 
 // Please note that you can use https://github.com/dotansimha/graphql-code-generator
 // to generate all types from graphQL schema
@@ -26,7 +25,6 @@ export default class extends React.Component<IndexPageProps, {}> {
           <strong>{this.props.data.site.siteMetadata.title}</strong> site.
         </p>
         <p>Now go build something great.</p>
-        <Link to="/page-2/">Go to page 2</Link>
       </div>
     )
   }
